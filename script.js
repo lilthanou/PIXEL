2539c916ab5df965d90b321216ee2b3b4586a5cb
lilthanou <85579409+lilthanou@users.noreply.github.com>
2025-04-19 16:56:11 +0200

    v3

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 gsap.registerPlugin(ScrollTrigger);
 
-for (let i = 0; i < sections.length+1; i++) {
-    gsap.to(sections[i], {
-        scrollTrigger: {
-            trigger: sections[i],
-            start: "top top",
-            endTrigger : "section" + (i+1),
-            scrub : true,
-        },
-        backgroundColor : colors[i+1],
-})};
+gsap.to(".second-pixel", {
+    scale: 2,
+  color : "#ff5424",
+  scrollTrigger: {
+    trigger: ".body",
+    start: "top",
+    end: "10% top",
+    scrub: true,
+    onLeave : () => {
+        document.querySelector(".second-pixel").style.width = "60dvw";
+        document.querySelector(".second-pixel").style.height = "60dvw";
+    },
+    onLeaveBack : () => {
+        document.querySelector(".second-pixel").style.width = "30dvw";
+        document.querySelector(".second-pixel").style.height = "30dvw";
+    },
+  }
+});
+
+gsap.to(".landing", {
+    alpha: 0,
+    scrollTrigger: {
+      trigger: ".landing",
+      start: "top",
+      end: "bottom top",
+      pin: true,
+      scrub: true,
+    }
+});
+
+gsap.to(".second-pixel", {
+    scale: 10,
+    xPercent: 7,
+    color: "#f4f1f1",
+    scrollTrigger: {
+      trigger: ".body",
+      start: "10% top",
+      end: "30% top",
+      markers: true,
+      scrub: true,
+    }
+});
