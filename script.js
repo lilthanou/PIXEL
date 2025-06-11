804d6528f918d46bc83ca147429838a5e0e39961
lilthanou <85579409+lilthanou@users.noreply.github.com>
2025-06-11 22:53:00 +0200

    UPDATE

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ gsap.registerPlugin(
   ScrollTrigger,
   ScrollSmoother,
   TextPlugin,
-  MotionPathPlugin
+  MotionPathPlugin,
+  DrawSVGPlugin
 );
 
 let menuOpen = false;
@@ -15,6 +16,8 @@ let chapterHeaders = document.querySelectorAll(".chapter-header");
 let thirdSectionTexts = document.querySelectorAll(".third-section-text-delete");
 let sectionSixthContainer = gsap.utils.toArray(".sixth-section-element");
 let sectionSixthTexts = gsap.utils.toArray(".sixth-section-text");
+let wavePoints = document.querySelectorAll(".wave-point");
+let tenthSectionCircles = document.querySelectorAll(".tenth-section-circle");
 
 function openMenu() {
   if (menuOpen) {
@@ -434,49 +437,49 @@ gsap.to(".third-section-text", {
     end: "bottom 50%",
     scrub: 1,
   },
-})
+});
 
-  gsap.to(".third-section", {
-    scrollTrigger: {
-      trigger: ".third-section",
-      start: "top",
-      end: "bottom",
-      pin: true,
-      scrub: true,
-      onLeave: () => {
-        gsap.to(".third-section", {
-          alpha: 0,
-        });
-        gsap.to(".fourth-section", {
-          alpha: 1,
-        });
-        changeBackgroundColor();
-      },
-      onEnterBack: () => {
-        gsap.to(".third-section", {
-          alpha: 1,
-        });
-        gsap.to(".fourth-section", {
-          alpha: 0,
-        });
-        changeBackgroundColor();
-      },
-      onEnter: () => {
-        gsap.to(".fourth-section", {
-          alpha: 0,
-          duration: 0,
-        });
-      },
-      onLeaveBack: () => {
-        gsap.to(".fourth-section", {
-          alpha: 1,
-        });
-        gsap.to(".third-section", {
-          alpha: 0,
-        });
-      },
+gsap.to(".third-section", {
+  scrollTrigger: {
+    trigger: ".third-section",
+    start: "top",
+    end: "bottom",
+    pin: true,
+    scrub: true,
+    onLeave: () => {
+      gsap.to(".third-section", {
+        alpha: 0,
+      });
+      gsap.to(".fourth-section", {
+        alpha: 1,
+      });
+      changeBackgroundColor();
     },
-  });
+    onEnterBack: () => {
+      gsap.to(".third-section", {
+        alpha: 1,
+      });
+      gsap.to(".fourth-section", {
+        alpha: 0,
+      });
+      changeBackgroundColor();
+    },
+    onEnter: () => {
+      gsap.to(".fourth-section", {
+        alpha: 0,
+        duration: 0,
+      });
+    },
+    onLeaveBack: () => {
+      gsap.to(".fourth-section", {
+        alpha: 1,
+      });
+      gsap.to(".third-section", {
+        alpha: 0,
+      });
+    },
+  },
+});
 
 // Fourth section
 
@@ -611,9 +614,6 @@ gsap.to(".transition-circle", {
         duration: 1,
       });
     },
-    onLeave: () => {
-      gsap.to(".circle-transition", {});
-    },
   },
 });
 
@@ -683,7 +683,7 @@ gsap.to(".seventh-section > .section-text", {
     trigger: ".seventh-section",
     start: "10% top",
     end: "20% top",
-    scrub: 1,
+    scrub: true,
   },
 });
 
@@ -701,7 +701,33 @@ gsap.to(".seventh-section > .section-text", {
   },
 });
 
+gsap.to(".seventh-section > .section-text", {
+  text: {
+    delimiter: "",
+    value:
+      "But what if images were made of waves too? <br><br>Fourier’s idea extends beyond sound. He showed that 2D signals, like images, are made of waves too.",
+  },
+  scrollTrigger: {
+    trigger: ".seventh-section",
+    start: "50% top",
+    end: "60% top",
+    scrub: 1,
+  },
+});
 
+gsap.to(".seventh-section > .section-text", {
+  text: {
+    delimiter: "",
+    value:
+      "These waves could build any image.But they were still analog, still infinite. <br><br>Someone had to bring them into the digital world.",
+  },
+  scrollTrigger: {
+    trigger: ".seventh-section",
+    start: "70% top",
+    end: "80% top",
+    scrub: 1,
+  },
+});
 
 gsap.to(".seventh-section", {
   scrollTrigger: {
@@ -709,6 +735,293 @@ gsap.to(".seventh-section", {
     start: "top",
     end: "bottom",
     pin: true,
+    scrub: 1,
+    onEnter: () => {
+      gsap.to(".chapter-three", {
+        alpha: 0,
+        duration: 0,
+      });
+    },
+    onLeave: () => {
+      gsap.to(".seventh-section", {
+        alpha: 0,
+      });
+      gsap.to(".chapter-three", {
+        alpha: 1,
+        duration: 0,
+      });
+      changeBackgroundColor();
+    },
+    onEnterBack: () => {
+      gsap.to(".seventh-section", {
+        alpha: 1,
+      });
+      gsap.to(".chapter-three", {
+        alpha: 0,
+      });
+      changeBackgroundColor();
+    },
+  },
+});
+
+// Chapter three
+
+gsap.to(".chapter-three", {
+  scrollTrigger: {
+    trigger: ".chapter-three",
+    start: "top",
+    end: "bottom",
+    pin: true,
+    scrub: 1,
+    onEnter: () => {
+      gsap.to(".eighth-section", {
+        alpha: 0,
+        duration: 0,
+      });
+    },
+    onLeave: () => {
+      gsap.to(".chapter-three", {
+        alpha: 0,
+      });
+      gsap.to(".eighth-section", {
+        alpha: 1,
+        duration: 0,
+      });
+    },
+    onEnterBack: () => {
+      gsap.to(".chapter-three", {
+        alpha: 1,
+      });
+      gsap.to(".eighth-section", {
+        alpha: 0,
+      });
+    },
+  },
+});
+
+// Eighth section
+
+gsap.from(".wave", {
+  drawSVG: "0% 0%",
+  scrollTrigger: {
+    trigger: ".eighth-section",
+    start: "top",
+    end: "20% top",
     scrub: true,
+    onEnter: () => {
+      document.querySelector(".eighth-section > p").innerHTML =
+        "In 1933, Vladimir Kotelnikov published the Sampling Theorem:<br /><br />He proved you could take a smooth continous wave ...";
+    },
+  },
+});
+
+gsap.from(".wave", {
+  drawSVG: "100% 0%",
+  scrollTrigger: {
+    trigger: ".eighth-section",
+    start: "30% top",
+    end: "40% top",
+    scrub: true,
+    onEnter: () => {
+      document.querySelector(".eighth-section > p").innerHTML =
+        "And if you only keep a few key points ...";
+      for (let i = 0; i < wavePoints.length; i++) {
+        gsap.to(wavePoints[i], {
+          alpha: 1,
+        });
+      }
+    },
+    onLeaveBack: () => {
+      document.querySelector(".eighth-section > p").innerHTML =
+        "In 1933, Vladimir Kotelnikov published the Sampling Theorem:<br /><br />He proved you could take a smooth continous wave ...";
+      gsap.to(wavePoints, {
+        alpha: 0,
+      });
+    },
+  },
+});
+
+gsap.from(".wave", {
+  scrollTrigger: {
+    trigger: ".eighth-section",
+    start: "45% top",
+    end: "55% top",
+    scrub: 1,
+    onEnter: () => {
+      document.querySelector(".eighth-section > p").innerHTML =
+        "Just the dots, and nothing else.";
+    },
+    onLeaveBack: () => {
+      document.querySelector(".eighth-section > p").innerHTML =
+        "And if you only keep a few key points ...";
+    },
+  },
+});
+
+gsap.from(".wave", {
+  drawSVG: 1,
+  scrollTrigger: {
+    trigger: ".eighth-section",
+    start: "60% top",
+    end: "70% top",
+    scrub: 1,
+    onEnter: () => {
+      document.querySelector(".eighth-section > p").innerHTML =
+        "You can rebuild the orinigal wave, and nothing was lost.<br> The original signal is fully rebuils, from just the samples.";
+    },
+    onLeaveBack: () => {
+      document.querySelector(".eighth-section > p").innerHTML =
+        "Just the dots, and nothing else.";
+    },
+  },
+});
+
+gsap.to(".eighth-section", {
+  scrollTrigger: {
+    trigger: ".eighth-section",
+    start: "top",
+    end: "bottom",
+    pin: true,
+    scrub: 1,
+    onEnter: () => {
+      gsap.to(".ninth-section", {
+        alpha: 0,
+      });
+    },
+    onLeave: () => {
+      gsap.to(".eighth-section", {
+        alpha: 0,
+      });
+      gsap.to(".ninth-section", {
+        alpha: 1,
+      });
+      changeBackgroundColor();
+    },
+    onEnterBack: () => {
+      gsap.to(".eighth-section", {
+        alpha: 1,
+      });
+      gsap.to(".ninth-section", {
+        alpha: 0,
+      });
+      changeBackgroundColor();
+    },
+  },
+});
+
+// Ninth section
+
+gsap.to(".ninth-section", {
+  scrollTrigger: {
+    trigger: ".ninth-section",
+    start: "top",
+    end: "bottom",
+    pin: true,
+    scrub: 1,
+    onEnter: () => {
+      gsap.to(".tenth-section", {
+        alpha: 0,
+      });
+    },
+    onLeave: () => {
+      gsap.to(".tenth-section", {
+        alpha: 1,
+      });
+      gsap.to(".ninth-section", {
+        alpha: 0,
+      });
+      changeBackgroundColor();
+    },
+    onEnterBack: () => {
+      gsap.to(".tenth-section", {
+        alpha: 0,
+      });
+      gsap.to(".ninth-section", {
+        alpha: 1,
+      });
+      changeBackgroundColor();
+    },
+  },
+});
+
+// Tenth section
+
+gsap.to(".tenth-section", {
+  scrollTrigger: {
+    trigger: ".tenth-section",
+    start: "20% top",
+    end: "40% top",
+    scrub: 1,
+    onEnter: () => {
+      for (let i = 0; i < tenthSectionCircles.length; i++) {
+        gsap.to(tenthSectionCircles[i], {
+          scale: Math.random() * 1.3 + 0.5,
+        });
+      }
+    },
+    onLeaveBack: () => {
+      for (let i = 0; i < tenthSectionCircles.length; i++) {
+        gsap.to(tenthSectionCircles[i], {
+          scale: 1,
+        });
+      }
+    },
+  },
+});
+
+gsap.to(tenthSectionCircles, {
+  /* backgroundImage : "radial-gradient(circle,rgba(255, 83, 36, 0.76) 30%,rgba(244, 241, 241, 0) 70%)", */
+  filter : "blur(5px)",
+  scrollTrigger: {
+    trigger: ".tenth-section",
+    start: "60% top",
+    end: "bottom",
+    scrub: true,
+    markers: true,
+  },
+});
+
+gsap.to(".tenth-section", {
+  scrollTrigger: {
+    trigger: ".tenth-section",
+    start: "top",
+    end: "bottom",
+    pin: true,
+    scrub: 1,
+    onEnter: () => {
+      gsap.to(".eleventh-section", {
+        alpha: 0,
+      });
+    },
+    onLeave: () => {
+      gsap.to(".eleventh-section", {
+        alpha: 1,
+      });
+      gsap.to(".tenth-section", {
+        alpha: 0,
+      });
+      changeBackgroundColor();
+    },
+    onEnterBack: () => {
+      gsap.to(".eleventh-section", {
+        alpha: 0,
+      });
+      gsap.to(".tenth-section", {
+        alpha: 1,
+      });
+      changeBackgroundColor();
+    },
+  },
+});
+
+//eleventh section
+
+gsap.to(".eleventh-section", {
+  scrollTrigger: {
+    trigger: ".eleventh-section",
+    start: "top",
+    end: "bottom",
+    pin: true,
+    scrub: 1,
   },
 });
